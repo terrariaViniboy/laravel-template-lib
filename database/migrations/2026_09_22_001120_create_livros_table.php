c1b74772a55f6c681baeb26a181e2118dd82d86e
terrariaViniboy <129099056+terrariaViniboy@users.noreply.github.com>
2026-09-21 22:04:49 -0300

    Completo

---
+++ database/migrations/2026_09_22_001120_create_livros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo');
+            $table->string('autor')->default('Desconhecido');
+            $table->string('ano_publicacao');
+            $table->string('isbn')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+};
